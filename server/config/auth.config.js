23599ebd24d9a6f15428c4974b49dbde569d6b6d
pddt1 <91549883+pddt1@users.noreply.github.com>
2022-05-13 21:05:48 +0700

    Add files via upload

---
+++ server/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "noteapp-secret-key"
+  };
---
+++ server/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports={
+    HOST: "localhost",
+    PORT: 27017,
+    DB: "noteapp"
+}
+//mongodb+srv://duytien:duytien@cluster0.lz3b0.mongodb.net/
