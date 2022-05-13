07c5b603edaa497323cce9fcf5ee7e0d0f6fc887
pddt1 <91549883+pddt1@users.noreply.github.com>
2022-05-13 17:33:05 +0700

    Delete server/config directory

--- server/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    // HOST: "localhost",
-    HOST:"duytien:duytien@cluster0.lz3b0.mongodb.net",
-    // PORT: 27017,
-    DB: "noteapp"
-}
-//mongodb+srv://duytien:duytien@cluster0.lz3b0.mongodb.net/
